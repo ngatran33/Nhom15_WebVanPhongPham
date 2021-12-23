491f0e044ee2556fce2124ae2a5547208b54b354
Trần Thúy Nga <ngat35137@gmail.com>
2021-12-23 22:47:29 +0700

    ip

---
+++ Nhom15_WebVanPhongPham/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nhom15_WebVanPhongPham.Models;
+
+namespace Nhom15_WebVanPhongPham.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBNhom15 db = new DBNhom15();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult ProductList()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Account()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }public ActionResult Card()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View(db.Blogs.ToList());
+        }
+        public ActionResult ChiTietBlogs(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+
+    }
+}
