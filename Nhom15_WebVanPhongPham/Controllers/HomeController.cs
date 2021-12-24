953ec6df707927348aa0c6ead8ed7e544fff597d
Trần Thúy Nga <ngat35137@gmail.com>
2021-12-24 15:30:12 +0700

    up

--- Nhom15_WebVanPhongPham/Controllers/HomeController.cs
+++ Nhom15_WebVanPhongPham/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace Nhom15_WebVanPhongPham.Controllers
     public class HomeController : Controller
     {
         DBNhom15 db = new DBNhom15();
+<<<<<<< Updated upstream
         public ActionResult Index()
         {
             return View();
@@ -22,6 +23,9 @@ namespace Nhom15_WebVanPhongPham.Controllers
 
             return View();
         }
+=======
+        
+>>>>>>> Stashed changes
 
         public ActionResult ProductList()
         {
@@ -58,6 +62,51 @@ namespace Nhom15_WebVanPhongPham.Controllers
             return View(blog);
         }
 
+<<<<<<< Updated upstream
 
+=======
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                sanPhams = db.SanPhams.Where(s => s.TenSP.Contains(searchString) || s.ThuongHieu.Contains(searchString)).Select(s => s).ToList();
+            }
+            //sắp xếp
+            switch (sortOder)
+            {
+                case "name_desc":
+                    sanPhams = sanPhams.OrderByDescending(s => s.TenSP).ToList();
+                    break;
+                case "Gia":
+                    sanPhams = sanPhams.OrderBy(s => s.Gia).ToList();
+                    break;
+                case "gia_desc":
+                    sanPhams = sanPhams.OrderByDescending(s => s.Gia).ToList();
+                    break;            
+                default:
+                    sanPhams = sanPhams.OrderBy(s => s.TenSP).ToList();
+                    break;
+            }
+            int pageSize = 8;
+            int pageNumber = (page ?? 1);
+            return View(sanPhams.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult ChiTietSP(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+        //public PartialViewResult _Searchbar()
+        //{
+        //    var sanPhams = db.SanPhams.Select(n => n);
+        //    return PartialView(sanPhams);
+        //}
+>>>>>>> Stashed changes
     }
 }
